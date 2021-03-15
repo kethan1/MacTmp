2b16a7d1938598760682ca95c6dcba6649ec3983
Kethan <kethan@vegunta.com>
2021-03-15 11:02:44 -0700

    Update __init__.py

--- MacTmp/__init__.py
+++ MacTmp/__init__.py
@@ -3,16 +3,16 @@ import os
 # ioreg -l |grep \"PerformanceStatistics\" | cut -d '{' -f 2 | tr '|' ',' | tr -d '}' | tr ',' '\n'|grep 'Temp\|Fan\|Clock'; done
 
 def CPU_Temp():
-    tmp = [each.strip() for each in (os.popen('sudo powermetrics --samplers smc -i1 -n1')).read().split('\n') if each != '']
-    for each in tmp:
-        if 'CPU die temperature' in tmp:
-            return each.strip('CPU die temperature: ')
+    cpu_temp = [each.strip() for each in (os.popen('sudo powermetrics --samplers smc -i1 -n1')).read().split('\n') if each != '']
+    for line in cpu_temp:
+        if 'CPU die temperature' in line:
+            return line.strip('CPU die temperature: ')
     return 'CPU Temperature not found'
 
 def GPU_Temp():
-    # Will not work with Integrated Intel Gpu
-    tmp = [each.strip() for each in (os.popen('sudo powermetrics --samplers smc -i1 -n1')).read().split('\n') if each != '']
-    for each in tmp:
-        if 'GPU die temperature' in tmp:
-            return each.strip('GPU die temperature: ')
+    # Will not work with Integrated Intel GPU
+    gpu_temp = [each.strip() for each in (os.popen('sudo powermetrics --samplers smc -i1 -n1')).read().split('\n') if each != '']
+    for line in gpu_temp:
+        if 'GPU die temperature' in line:
+            return line.strip('GPU die temperature: ')
     return 'GPU Temperature not found'
